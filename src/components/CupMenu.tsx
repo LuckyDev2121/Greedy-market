c21bfaf495a2d6ff05d0d892fef2353542cd41bf
Nana <anna021210@outlook.com>
2026-04-24 09:08:08 +0800

    update 335

--- src/components/CupMenu.tsx
+++ src/components/CupMenu.tsx
@@ -243,27 +243,27 @@ export default function CupMenu({ onCloseCup }: CupMenuProps) {
                 <div className="absolute flex items-center top-[480px] left-1/2 -translate-x-1/2 w-[346px] h-[48px] bg-gradient-to-br bg-[#fcbd5f] rounded-[9px]">
                     {myRanking?.data.ranking_position && myRanking?.data.ranking_position === 1 && (
                         <div className="h-[48px] w-[48px] bg-[#ffaa2c] rounded-l-[9px]">
-                            <img src={getAssetUrl(GAME_ASSETS.prize1)} alt="prize" className=" absolute h-[45px] w-[45px] left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2" />
+                            <img src={getAssetUrl(GAME_ASSETS.prize1)} alt="prize" className=" absolute h-[45px] w-[45px] top-1/2 " />
                         </div>
                     )}
                     {myRanking?.data.ranking_position && myRanking?.data.ranking_position === 2 && (
                         <div className="h-[48px] w-[48px] bg-[#ffaa2c] rounded-l-[9px]">
-                            <img src={getAssetUrl(GAME_ASSETS.prize2)} alt="prize" className=" absolute h-[45px] w-[45px] left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2" />
+                            <img src={getAssetUrl(GAME_ASSETS.prize2)} alt="prize" className=" absolute h-[45px] w-[45px] top-1/2 -translate-y-1/2 " />
                         </div>
                     )}
                     {myRanking?.data.ranking_position && myRanking?.data.ranking_position === 3 && (
                         <div className="h-[48px] w-[48px] bg-[#ffaa2c] rounded-l-[9px]">
-                            <img src={getAssetUrl(GAME_ASSETS.prize3)} alt="prize" className=" absolute h-[45px] w-[45px] left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2" />
+                            <img src={getAssetUrl(GAME_ASSETS.prize3)} alt="prize" className=" absolute h-[45px] w-[45px] top-1/2 -translate-y-1/2 " />
                         </div>
                     )}
                     {myRanking?.data.ranking_position && myRanking?.data.ranking_position > 3 && myRanking?.data.ranking_position < 99 && (
                         <div className="absolute left-0 top-0 h-[48px] w-[48px] bg-[#ffaa2c] rounded-l-[9px]">
-                            <span className="absolute text-[#FDF4C1]  font-bold text-[16px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 [text-shadow:1px_0_0_brown,-1px_0_0_brown,0_1px_0_brown,0_-1px_0_brown]">{myRanking?.data.ranking_position}</span>
+                            <span className="absolute text-[#FDF4C1]  font-bold text-[16px] top-1/2 -translate-y-1/2  [text-shadow:1px_0_0_brown,-1px_0_0_brown,0_1px_0_brown,0_-1px_0_brown]">{myRanking?.data.ranking_position}</span>
                         </div>
                     )}
                     {myRanking?.data.ranking_position && myRanking?.data.ranking_position > 98 && (
                         <div className="absolute left-0 top-0 h-[48px] w-[48px] bg-[#ffaa2c] rounded-l-[9px]">
-                            <span className="absolute text-[#FDF4C1]  font-bold text-[16px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 [text-shadow:1px_0_0_brown,-1px_0_0_brown,0_1px_0_brown,0_-1px_0_brown]">+99</span>
+                            <span className="absolute text-[#FDF4C1]  font-bold text-[16px] top-1/2 -translate-y-1/2  [text-shadow:1px_0_0_brown,-1px_0_0_brown,0_1px_0_brown,0_-1px_0_brown]">+99</span>
                         </div>
                     )}
                     <img src={resolveAssetUrl(playerInfo?.avater ?? "")} alt="avatar" className="absolute left-[50px]  h-[45px] w-[45px] rounded-full" />
